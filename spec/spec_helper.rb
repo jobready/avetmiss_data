1fa530a5af6d804018409b9b62574a09b7bfd607
Brenton "B-Train" Fletcher <i@bloople.net>
2014-03-18 16:22:11 +1100

    Fix code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,16 +5,14 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'bundler/setup'
-Bundler.require(:default, :development)
-
 require 'simplecov'
 SimpleCov.start
 
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
-require 'avetmiss_data'
+require 'bundler/setup'
+Bundler.require(:default, :development)
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
