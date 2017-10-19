6e5bf8132dd15e3776707addfdb711ce9ec9e612
Michael MacDonald <michael@starclass.com.au>
2017-10-19 17:35:54 +1100

    [AV-4072] Reduce line length of RTO store from 268 to 110

--- lib/avetmiss_data/stores/v8/rto.rb
+++ lib/avetmiss_data/stores/v8/rto.rb
@@ -2,7 +2,7 @@ class AvetmissData::Stores::V8::Rto < AvetmissData::Stores::V8::Base
   nat_file('NAT00010', {
     training_organisation_identifier: 0...10,
     training_organisation_name: 10...110,
-    extras: 268..-1
+    extras: 110..-1
   })
 
   alias_method :identifier, :training_organisation_identifier
